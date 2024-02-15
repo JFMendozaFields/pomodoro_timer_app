41ea918361c55c9ff92ba581521990e742264b2a
Okami-Gaijin <78839188+Okami-Gaijin@users.noreply.github.com>
2024-02-15 18:19:43 -0500

    created a stopwatch using python

---
+++ stopwatch.py
@@ -0,0 +1,34 @@
+import time, datetime
+
+#Stopwatch
+
+#Timer starter
+starttime = time.time()
+lasttime = starttime
+lapnum = 1
+value = ''
+
+
+print("Press ENTER for each lap. \n Type Q and press ENTER to stop.")
+
+while value.lower() != 'q':
+
+    # Input for the ENTER key press
+    value = input()
+
+    # Current lap time
+
+    laptime = round((time.time() - lasttime), 2)
+
+    # Total time since timer started
+    total_time = round((time.time() - starttime), 2)
+
+    #Printing the lap number, lap itme, and total time
+    print("Lap No. "+str(lapnum))
+    print("Lap Time: "+str(laptime))
+    print("Total Time: "+str(total_time))
+
+    #Updating the previous total time and lap number
+    lasttime = time.time()
+    lapnum += 1
+print("Excersice complete")
---
+++ time_intro.py
@@ -0,0 +1,26 @@
+import time 
+import datetime
+
+
+#time function being called by time module, returning time in epoch
+seconds = time.time()
+
+# ctime function called to convert epoch to local time
+local_time = time.ctime(seconds)
+print(
+    "epoch: ", seconds
+)
+#creates delay of 5 seconds before printing local time
+time.sleep(5)
+print(
+    "Local Time: ", local_time
+)
+
+#shows the current date and time using now attribute
+current = datetime.datetime.now()
+print(str(current))
+#shows time and date for a year from now using the timedelta attribute
+one_year = current + datetime.timedelta(days = 365)
+print(str(one_year))
+
+
